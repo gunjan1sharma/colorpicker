ef85930dfd7e9cad81db1ccec107d452f8ae486b
Gunjan Sharma <gunjansharma1112info@yahoo.com>
2023-12-26 18:48:57 +0530

    Basic bootstrapping done..

---
+++ src/components/PickerCard.tsx
@@ -0,0 +1,9 @@
+function PickerCard(props: any) {
+  return (
+    <div>
+      <h1>PickerCard</h1>
+    </div>
+  );
+}
+
+export default PickerCard;
---
+++ src/components/UpMenu.tsx
@@ -0,0 +1,5 @@
+function UpMenu(props:any) {
+    return (<div><h1>UpMenu</h1></div>)
+}
+
+export default UpMenu;
